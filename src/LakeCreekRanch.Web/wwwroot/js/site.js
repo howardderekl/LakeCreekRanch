c300c25c4e74b8097dd006355eaeeaff4350f55a
Derek Howard <derek.howard@intellitect.com>
2017-11-09 17:39:47 -0800

    Added jquery easing fo the scrolling Added a header welcome text.

--- src/LakeCreekRanch.Web/wwwroot/js/site.js
+++ src/LakeCreekRanch.Web/wwwroot/js/site.js
@@ -1,1 +1,19 @@
-﻿// Write your Javascript code.
+﻿$(function () {
+    $(window).on("load resize", function () {
+        $(".fill-screen").css("height", window.innerHeight - 50);
+    });
+
+    //Add Bootstraps scrollspy
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: 100
+    });
+
+    //Smooth Scrolling
+    $('nav a, .down-button a').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top - 75
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
