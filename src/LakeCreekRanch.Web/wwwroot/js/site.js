9b8d7f19aa15a5d25f1dbddafea09b1485c59cbc
Derek Howard <howardderekl@gmail.com>
2018-01-31 22:01:21 -0800

    Added a map of the property and a new publish profile.

--- src/LakeCreekRanch.Web/wwwroot/js/site.js
+++ src/LakeCreekRanch.Web/wwwroot/js/site.js
@@ -19,4 +19,46 @@
 
     // parallax scrolling with stellar.js
     $(window).stellar();
+
+    // Website Map to the property
+    function initMap() {
+
+        var location = new google.maps.LatLng(48.362152, -115.837865);
+
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+            center: location,
+            zoom: 16,
+            panControl: false,
+            mapTypeId: 'hybrid'
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+
+        var markerImage = '../images/map-marker-icon.png';
+
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            icon: markerImage
+        });
+
+        var contentString = '<div class="info-window">' +
+            '<h3>Lake Creek Ranch</h3>' +
+            '<div class="info-content">' +
+            '<p>65 Lakota Loop, Troy, MT 59935.  Lake Creek Ranch is a gated community of custom 3 to 15 acre home sites located near Troy Montana in the beautiful Bull Lake Valley.</p>' +
+            '</div>' +
+            '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 400
+        });
+
+        marker.addListener('click', function () {
+            infowindow.open(map, marker);
+        });
+
+    }
+
+    google.maps.event.addDomListener(window, 'load', initMap);
 });
